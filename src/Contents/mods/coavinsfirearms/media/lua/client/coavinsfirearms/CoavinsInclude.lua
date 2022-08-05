abb3f1288b2c4f0646dd228fc9b500e3ac2d1eb7
coavins <me@coavins.org>
2022-08-05 16:01:59 -0400

    Change model names to make things more clear
    By calling them 'GenericPistol' instead of 'Pistol', it should hopefully
    make it more clear that the model is a generic pistol model, and not the
    only way to represent pistols in the game.

--- src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/CoavinsInclude.lua
+++ src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/CoavinsInclude.lua
@@ -1,13 +1,13 @@
 -- To add support for your custom firearms, copy this file
 -- and match the types your mod is using to the appropriate model
 
-CoavinsFirearms:Include('Base.Pistol',         'Pistol')
-CoavinsFirearms:Include('Base.Pistol2',        'Pistol')
-CoavinsFirearms:Include('Base.Pistol3',        'Pistol')
-CoavinsFirearms:Include('Base.Revolver',       'Revolver')
-CoavinsFirearms:Include('Base.Revolver_Short', 'Revolver')
-CoavinsFirearms:Include('Base.Revolver_Long',  'Revolver')
-CoavinsFirearms:Include('Base.Shotgun',                    'Shotgun')
-CoavinsFirearms:Include('Base.ShotgunSawnoff',             'Shotgun')
-CoavinsFirearms:Include('Base.DoubleBarrelShotgun',        'Shotgun')
-CoavinsFirearms:Include('Base.DoubleBarrelShotgunSawnoff', 'Shotgun')
+CoavinsFirearms:Include('Base.Pistol',         'GenericPistol')
+CoavinsFirearms:Include('Base.Pistol2',        'GenericPistol')
+CoavinsFirearms:Include('Base.Pistol3',        'GenericPistol')
+CoavinsFirearms:Include('Base.Revolver',       'GenericRevolver')
+CoavinsFirearms:Include('Base.Revolver_Short', 'GenericRevolver')
+CoavinsFirearms:Include('Base.Revolver_Long',  'GenericRevolver')
+CoavinsFirearms:Include('Base.Shotgun',                    'GenericShotgun')
+CoavinsFirearms:Include('Base.ShotgunSawnoff',             'GenericShotgun')
+CoavinsFirearms:Include('Base.DoubleBarrelShotgun',        'GenericShotgun')
+CoavinsFirearms:Include('Base.DoubleBarrelShotgunSawnoff', 'GenericShotgun')
