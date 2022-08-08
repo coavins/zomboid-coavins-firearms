3ce2d862cf5f910d0fd777638ab3cbc25b1c564d
coavins <me@coavins.org>
2022-08-08 14:26:52 -0400

    Clean up comments

--- src/Contents/mods/coavinsfirearms/media/lua/server/CoavinsFirearmsDistributions.lua
+++ src/Contents/mods/coavinsfirearms/media/lua/server/CoavinsFirearmsDistributions.lua
@@ -3,55 +3,51 @@ require 'Items/ProceduralDistributions'
 local spawns = {}
 local debugMultiplier = 1.0 -- change to 100 to guarantee spawns
 
--- Defines a distribution with specified name and base rarity
+-- Make a new set of items and where they spawn
+local MakeSet = function(distribution, items)
+	table.insert(spawns, { distribution, items })
+end
+
+-- Define a place where things can spawn, with specified name and base rarity
 local MakeDistribution = function(name, rarity)
 	return { name, rarity }
 end
 
--- Defines an item of specified type and rarity multiplier
+-- Define an item that can spawn, with specified type and rarity multiplier
 local MakeItem = function(type, rarityMultiplier)
 	return {type, rarityMultiplier }
-end
-
-local MakeSet = function(distribution, items)
-	table.insert(spawns, { distribution, items })
 end
 
 -- Add spawn sets
 
 -- Gun parts
 MakeSet(
-{ -- Where they should spawn
-	MakeDistribution('PawnShopGunsSpecial' , 5.0),
-	MakeDistribution('GunStoreShelf'       , 5.0),
-	MakeDistribution('ArmyStorageGuns'     , 0.5),
-	MakeDistribution('ArmySurplusBackpacks', 1.0),
-	MakeDistribution('LockerArmyBedroom'   , 5.0),
-	MakeDistribution('PoliceStorageGuns'   , 2.0),
-	MakeDistribution('GunStoreDisplayCase' , 0.1),
-	MakeDistribution('FirearmWeapons'      , 1.0),
-	MakeDistribution('HuntingLockers'      , 2.0),
-},
-{ -- What should spawn here
-	--MakeItem('coavinsfirearms.PistolReceiver'    , 1.0),
-	MakeItem('coavinsfirearms.PistolSlide'       , 1.0),
-	MakeItem('coavinsfirearms.PistolBarrel'      , 1.0),
-	--MakeItem('coavinsfirearms.RevolverReceiver'  , 1.0),
-	MakeItem('coavinsfirearms.RevolverCylinder'  , 1.0),
-	--MakeItem('coavinsfirearms.BoltActionReceiver', 1.0),
-	MakeItem('coavinsfirearms.BoltActionBolt'    , 1.0),
-	--MakeItem('coavinsfirearms.M16LowerReceiver'  , 1.0),
-	MakeItem('coavinsfirearms.M16UpperReceiver'  , 1.0),
-	MakeItem('coavinsfirearms.M16BoltCarrier'    , 1.0),
-	MakeItem('coavinsfirearms.M16FiringPin'      , 1.0),
-	MakeItem('coavinsfirearms.M16Bolt'           , 1.0),
-	--MakeItem('coavinsfirearms.ShotgunReceiver'   , 1.0),
-	MakeItem('coavinsfirearms.ShotgunForend'     , 1.0),
-	MakeItem('coavinsfirearms.ShotgunBoltCarrier', 1.0),
-	MakeItem('coavinsfirearms.ShotgunBolt'       , 1.0),
-	MakeItem('coavinsfirearms.ShotgunBarrel'     , 1.0),
-	
-})
+	{ -- Where they should spawn
+		MakeDistribution('PawnShopGunsSpecial' , 5.0),
+		MakeDistribution('GunStoreShelf'       , 5.0),
+		MakeDistribution('ArmyStorageGuns'     , 0.5),
+		MakeDistribution('ArmySurplusBackpacks', 1.0),
+		MakeDistribution('LockerArmyBedroom'   , 5.0),
+		MakeDistribution('PoliceStorageGuns'   , 2.0),
+		MakeDistribution('GunStoreDisplayCase' , 0.1),
+		MakeDistribution('FirearmWeapons'      , 1.0),
+		MakeDistribution('HuntingLockers'      , 2.0),
+	},
+	{ -- What should spawn here
+		MakeItem('coavinsfirearms.PistolSlide'       , 1.0),
+		MakeItem('coavinsfirearms.PistolBarrel'      , 0.7),
+		MakeItem('coavinsfirearms.RevolverCylinder'  , 1.0),
+		MakeItem('coavinsfirearms.BoltActionBolt'    , 0.7),
+		MakeItem('coavinsfirearms.M16UpperReceiver'  , 1.0),
+		MakeItem('coavinsfirearms.M16BoltCarrier'    , 1.0),
+		MakeItem('coavinsfirearms.M16FiringPin'      , 0.5),
+		MakeItem('coavinsfirearms.M16Bolt'           , 0.7),
+		MakeItem('coavinsfirearms.ShotgunForend'     , 1.0),
+		MakeItem('coavinsfirearms.ShotgunBoltCarrier', 1.0),
+		MakeItem('coavinsfirearms.ShotgunBolt'       , 0.7),
+		MakeItem('coavinsfirearms.ShotgunBarrel'     , 0.7),
+	}
+)
 
 -- If adding a new spawn set, do it here
 
