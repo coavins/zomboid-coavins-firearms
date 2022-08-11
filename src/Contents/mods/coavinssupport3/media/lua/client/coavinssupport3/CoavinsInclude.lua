fdb05fe2eccb96d3d6719e22b04974dfe8af16c9
coavins <me@coavins.org>
2022-08-11 19:02:21 -0400

    Add coavinssupport3
    Compatibility with Vanilla Firearms Expanded

---
+++ src/Contents/mods/coavinssupport3/media/lua/client/coavinssupport3/CoavinsInclude.lua
@@ -0,0 +1,32 @@
+-- To add support for your custom firearms, copy this file
+-- and match the types your mod is using to the appropriate model
+
+CoavinsFirearms.Include('Base.Shotgun',                    'GenericShotgun')
+CoavinsFirearms.Include('Base.ShotgunSawnoff',             'GenericShotgun')
+CoavinsFirearms.Include('Base.ShotgunSawnoffNoStock',      'GenericShotgun')
+CoavinsFirearms.Include('Base.DoubleBarrelShotgun',        'GenericShotgun')
+CoavinsFirearms.Include('Base.DoubleBarrelShotgunSawnoff', 'GenericShotgun')
+CoavinsFirearms.Include('Base.Spas12',                     'GenericShotgun')
+CoavinsFirearms.Include('Base.Spas12Folded',               'GenericShotgun')
+--CoavinsFirearms.Include('Base.SKS',          '')
+--CoavinsFirearms.Include('Base.1022',         '')
+--CoavinsFirearms.Include('Base.Mini14',       '')
+--CoavinsFirearms.Include('Base.Mini14Folded', '')
+CoavinsFirearms.Include('Base.CAR15',         'M16Rifle')
+CoavinsFirearms.Include('Base.CAR15D',        'M16Rifle')
+CoavinsFirearms.Include('Base.AssaultRifle',  'M16Rifle')
+CoavinsFirearms.Include('Base.AssaultRifle2', 'M16Rifle')
+CoavinsFirearms.Include('Base.HuntingRifle',  'BoltActionRifle')
+CoavinsFirearms.Include('Base.VarmintRifle',  'BoltActionRifle')
+--CoavinsFirearms.Include('Base.LeverRifle', '') -- W1866 Carbine
+--CoavinsFirearms.Include('Base.MP5', '')
+--CoavinsFirearms.Include('Base.MP5SD', '')
+--CoavinsFirearms.Include('Base.Tec9', '')
+CoavinsFirearms.Include('Base.Pistol2',        'GenericPistol')
+CoavinsFirearms.Include('Base.Pistol',         'GenericPistol')
+CoavinsFirearms.Include('Base.Glock',          'GenericPistol')
+CoavinsFirearms.Include('Base.Revolver_Short', 'GenericRevolver')
+CoavinsFirearms.Include('Base.Revolver_Long',  'GenericRevolver')
+CoavinsFirearms.Include('Base.Pistol3',        'GenericPistol')
+CoavinsFirearms.Include('Base.Revolver',       'GenericRevolver')
+
