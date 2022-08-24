fd316d98e7136748ce350be4818086b646c2c0d6
coavins <me@coavins.org>
2022-08-24 16:11:31 -0400

    Fix exception when weapon breaks after swinging it

--- src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/OnShoot.lua
+++ src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/OnShoot.lua
@@ -1,7 +1,7 @@
 local FIREARMS = require('coavinsfirearms/FirearmsHelper')
 
 local function OnPlayerAttackFinished(player, handWeapon)
-	if FIREARMS.itemIsFirearm(handWeapon) then
+	if handWeapon and FIREARMS.itemIsFirearm(handWeapon) then
 		-- first, get the amount of condition lost from this attack
 		local lowerChance = handWeapon:getConditionLowerChance()
 		local maintenanceMod = player:getMaintenanceMod()
@@ -20,7 +20,7 @@ local function OnPlayerAttackFinished(player, handWeapon)
 		-- if any loss was incurred, each part will handle its own calculation
 		FIREARMS.updateFirearm(handWeapon, conditionLoss)
 	end
-	print(string.format('Condition is %.2f', handWeapon:getCondition()))
+	--print(string.format('Condition is %.2f', handWeapon:getCondition()))
 end
 
 Events.OnPlayerAttackFinished.Add(OnPlayerAttackFinished)
