76e52f3772d71cac18f795e60b197e3b143971fa
coavins <me@coavins.org>
2023-05-24 12:11:39 -0400

    Add sandbox option for condition loss rate

--- src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/OnShoot.lua
+++ src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/OnShoot.lua
@@ -1,15 +1,23 @@
 local FIREARMS = require('coavinsfirearms/FirearmsHelper')
 
+local ConditionLossChance = {
+	0.5,
+	1.0,
+	1.5,
+	2.0
+}
+
 local function OnPlayerAttackFinished(player, handWeapon)
 	if handWeapon and FIREARMS.itemIsFirearm(handWeapon) then
 		-- first, get the amount of condition lost from this attack
 		local lowerChance = handWeapon:getConditionLowerChance()
 		local maintenanceMod = player:getMaintenanceMod()
+		local lossModifier = ConditionLossChance[SandboxVars.coavinsfirearms.ConditionLossChance]
 		local conditionLoss = 0
 
-		--print(string.format('1: %.2f, 2: %.2f', lowerChance, maintenanceMod))
+		print(string.format('1: %.2f, 2: %.2f, 3: %.2f', lowerChance, lossModifier, maintenanceMod))
 
-		local chance = lowerChance + (maintenanceMod * 2)
+		local chance = (lowerChance * lossModifier) + (maintenanceMod * 2)
 
 		if ZombRand(chance) == 0 then
 			-- weapon sustained damage
