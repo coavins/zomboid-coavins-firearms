1a18e44307e9a46250297303ff1ce784ea2fb95b
macinsight <gh@macinsight.net>
2023-11-27 22:11:40 +0100

    64 add model shotgun 410g (#72)
    * Add model def for #64
    
    Signed-off-by: macinsight <gh@macinsight.net>
    
    * Add Help defs for #64
    
    Signed-off-by: macinsight <gh@macinsight.net>
    
    * Add lang defs for #64
    
    Signed-off-by: macinsight <gh@macinsight.net>
    
    * Add fix defs for #64
    
    Signed-off-by: macinsight <gh@macinsight.net>
    
    * Add item defs for #64
    
    Signed-off-by: macinsight <gh@macinsight.net>
    
    * Update coavins_fixing_shotgun_410g.txt
    
    Adjust item types from shotgun_10g to shotgun_410g where missed
    
    * Update coavins_items_shotgun_410g.txt
    
    Change types from 4410g to 410g where required
    
    ---------
    
    Signed-off-by: macinsight <gh@macinsight.net>

--- src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/CoavinsModels.lua
+++ src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/CoavinsModels.lua
@@ -88,6 +88,10 @@ CoavinsFirearms.AddOrReplaceModel(
 , { 'RevolverReceiver_38spc', 'RevolverCylinder_38spc' }
 , 'RevolverReceiver_38spc')
 CoavinsFirearms.AddOrReplaceModel(
+	'Shotgun_410g'
+, { 'ShotgunReceiver_410g', 'ShotgunBarrel_410g' }
+, 'ShotgunReceiver_410g')
+CoavinsFirearms.AddOrReplaceModel(
 	'Shotgun_20g'
 , { 'ShotgunReceiver_20g', 'ShotgunBarrel_20g' }
 , 'ShotgunReceiver_20g')
