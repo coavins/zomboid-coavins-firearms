489f9bb43bbd79a845f0a188fa45a479005fbdb2
macinsight <gh@macinsight.net>
2023-11-29 15:43:03 +0100

    fix(model): Remove .50 AE from Models

--- src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/CoavinsModels.lua
+++ src/Contents/mods/coavinsfirearms/media/lua/client/coavinsfirearms/CoavinsModels.lua
@@ -25,52 +25,42 @@ CoavinsFirearms.AddOrReplaceModel(
 , { 'BoltActionReceiver', 'BoltActionBolt' }
 , 'BoltActionReceiver'
 , 'Base.VarmintRifle')
-
 CoavinsFirearms.AddOrReplaceModel(
 	'SKS'
 , { 'SKS_Receiver', 'SKS_BoltCarrier' }
 , 'SKS_Receiver')
-
 CoavinsFirearms.AddOrReplaceModel(
 	'AK47'
 , { 'AK47_Receiver', 'AK47_BoltCarrier' }
 , 'AK47_Receiver')
-
 CoavinsFirearms.AddOrReplaceModel(
   'Pistol_45acp'
 , { 'PistolReceiver_45acp', 'PistolSlide_45acp'}
 , 'PistolReceiver_45acp')
-
 CoavinsFirearms.AddOrReplaceModel(
   'Pistol_9mm'
 , { 'PistolReceiver_9mm', 'PistolSlide_9mm'}
 , 'PistolReceiver_9mm')
-
 CoavinsFirearms.AddOrReplaceModel(
 	'Shotgun_10g'
 , { 'ShotgunReceiver_10g', 'ShotgunBarrel_10g' }
 , 'ShotgunReceiver_10g')
-
 CoavinsFirearms.AddOrReplaceModel(
 	'Shotgun_12g'
 , { 'ShotgunReceiver_12g', 'ShotgunBarrel_12g' }
 , 'ShotgunReceiver_12g')
-
 CoavinsFirearms.AddOrReplaceModel(
   'Pistol_22lr'
 , { 'PistolReceiver_22lr', 'PistolSlide_22lr'}
 , 'PistolReceiver_22lr')
-
 CoavinsFirearms.AddOrReplaceModel(
 	'Revolver_45lc'
 , { 'RevolverReceiver_45lc', 'RevolverCylinder_45lc' }
 , 'RevolverReceiver_45lc')
-
 CoavinsFirearms.AddOrReplaceModel(
   'Pistol_380acp'
 , { 'PistolReceiver_380acp', 'PistolSlide_380acp'}
 , 'PistolReceiver_380acp')
-
 CoavinsFirearms.AddOrReplaceModel(
 	'Rifle_556'
 , { 'RifleUpperReceiver_556', 'RifleLowerReceiver_556' }
@@ -105,7 +95,3 @@ CoavinsFirearms.AddOrReplaceModel(
 'Shotgun_4g'
 , { 'ShotgunReceiver_4g', 'ShotgunBarrel_4g' }
 , 'ShotgunReceiver_4g')
-CoavinsFirearms.AddOrReplaceModel(
-  'Pistol_50ae'
-, { 'PistolReceiver_50ae', 'PistolSlide_50ae'}
-, 'PistolReceiver_50ae')
